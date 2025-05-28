cddde88e3f808f094d6e652cd9dedc3aa07e529f
Seph De Busser <seph.debusser@gmail.com>
2025-05-28 18:34:37 +0200

    Deployed e75f473 with MkDocs version: 1.6.1

--- do-graphs.js
+++ do-graphs.js
@@ -1,7 +1,6 @@
 let dots = document.getElementsByClassName("language-dot");
 [...dots].forEach(e => {
     Viz.instance().then(viz => {
-        e.parentElement.style.textAlign = "center";
         e.replaceWith(viz.renderSVGElement(e.textContent));
     });
 });
